7773ddc39eb455581e137f2c4845f85cd3923806
Kay <k.dgray23@gmail.com>
2016-08-23 22:00:33 -0400

    Update multiple_user_mbx_permissions.ps1

--- multiple_user_mbx_permissions.ps1
+++ multiple_user_mbx_permissions.ps1
@@ -1,7 +1,7 @@
-﻿#$users = @('Kristin Swinehart','Renee Baker','Scott Burke','Carol Tang','Daniel Ehrlichman')
+﻿#$users = @('')
 
 #foreach($user in $users) {
- #Add-MailboxPermission "Financial Explorers - Americas" -User $user -AccessRights fullaccess -InheritanceType all   
+ #Add-MailboxPermission "" -User $user -AccessRights fullaccess -InheritanceType all   
 #}
 
 
