d9b277d48973b2eb6ecc4aa526be4924a342f5b7
Kay <k.dgray23@gmail.com>
2016-08-23 21:43:26 -0400

    Update wmi_end_process.ps1

--- wmi_end_process.ps1
+++ wmi_end_process.ps1
@@ -1,4 +1,4 @@
-﻿(gwmi win32_process -ComputerName phl107762 -Filter "name='acrobat.exe'").terminate()
+﻿(gwmi win32_process -ComputerName  -Filter "name='acrobat.exe'").terminate()
 
 ping www.hackthissite.org -f -l 1300
 tracert hackthissite.org
