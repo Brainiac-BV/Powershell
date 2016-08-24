e76a8b289344c2f2c0b543db97bb4ab2ba1236f2
Kay <k.dgray23@gmail.com>
2016-08-23 21:48:54 -0400

    Update remote_install.ps1

--- remote_install.ps1
+++ remote_install.ps1
@@ -1,6 +1,6 @@
 ﻿#$cmd = "c:\windows\system32\msiexec.exe"
-#$path = "c:\temp\AcroRead.msi"
-$script = {Invoke-Expression "msiexec.exe /i c:\temp\AcroRead.msi /quiet /norestart"}
+#$path = ''
+$script = {Invoke-Expression "msiexec.exe /i c:\ /quiet /norestart"}
 
-Invoke-Command -ComputerName phl107763 -ScriptBlock $script
+Invoke-Command -ComputerName  -ScriptBlock $script
 
