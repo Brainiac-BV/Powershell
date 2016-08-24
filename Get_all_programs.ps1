e3c116d32fd1933f2ba9364150978a961f0f8fc3
Kay <k.dgray23@gmail.com>
2016-08-23 21:29:59 -0400

    Update Get_all_programs.ps1

--- Get_all_programs.ps1
+++ Get_all_programs.ps1
@@ -1,4 +1,4 @@
 ﻿$regloc = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
 $regloc64 = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
 
-Invoke-Command -ComputerName phl10621 -ScriptBlock {Get-ChildItem $using:regloc64 -Recurse|sort -Descending |ForEach-Object -Process {$_.GetValue("DisplayName"), $_.GETVAlue("uninstall string")}| Format-Table}
+Invoke-Command -ComputerName -ScriptBlock {Get-ChildItem $using:regloc64 -Recurse|sort -Descending |ForEach-Object -Process {$_.GetValue("DisplayName"), $_.GETVAlue("uninstall string")}| Format-Table}
