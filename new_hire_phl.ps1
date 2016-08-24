0dc8773c7f5bf0a0a8f75d43db0ebb7cbd7b7b3d
Kay <k.dgray23@gmail.com>
2016-08-23 21:58:58 -0400

    Update new_hire_phl.ps1

--- new_hire_phl.ps1
+++ new_hire_phl.ps1
@@ -1,11 +1,11 @@
-﻿$user = "CN=Otis Aracena,OU=Philadelphia Market Street,OU=Users,OU=User Accounts,DC=aberdeen,DC=aberdeen-asset,DC=com"
-$oldOU = "CN=Stacy Lee,OU=new,OU=User Accounts,DC=aberdeen,DC=aberdeen-asset,DC=com"
-$newOU = "OU=Philadelphia Market Street,OU=Users,OU=User Accounts,DC=aberdeen,DC=aberdeen-asset,DC=com"
-$group1 =  "remote users phl"
-$group2 =  "outlook users us"
-$group3 =  "EVSTDArchivingUS"
-$group4 =  "xp users"
-$group5 =  "philadelphia users"
+﻿$user = "CN=,OU=,OU=Users,OU=User Accounts,DC=,DC=,DC="
+$oldOU = "CN=,OU=new,OU=User Accounts,DC=,DC=,DC="
+$newOU = "OU=,OU=Users,OU=User Accounts,DC=,DC=,DC="
+$group1 =  ""
+$group2 =  ""
+$group3 =  ""
+$group4 =  ""
+$group5 =  ""
 
 #Move-QADObject $oldOU -NewParentContainer $newOU
 #Add-QADGroupMember $group1 $user
@@ -14,7 +14,7 @@ $group5 =  "philadelphia users"
 #Add-QADGroupMember $group4 $user
 #Add-QADGroupMember $group5 $user
 
-#Set-QADUser $user -Description "StacyLee$" -HomeDirectory \\phlfile3\StacyLee$ -HomeDrive u:
+#Set-QADUser $user -Description "" -HomeDirectory \\server)\ -HomeDrive u:
 
 
 
