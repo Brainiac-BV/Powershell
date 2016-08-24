a23e4e789e282f53f41ef78be67d5f532e734dcd
Kay <k.dgray23@gmail.com>
2016-08-23 22:08:25 -0400

    Update java_uninstall.ps1

--- java_uninstall.ps1
+++ java_uninstall.ps1
@@ -1,3 +1,3 @@
 ﻿$msiPath = "msiexec.exe"
 $msiargs = "/X {88888888-FFFF-0000-FFFF-000055551111} /quiet /norestart"
-Invoke-Command -ComputerName phl12432 -Credential aberdeen\admin-kg1 -ScriptBlock {Start-Process -FilePath $args[0] -ArgumentList "/X {88888888-FFFF-0000-FFFF-000055551111} /quiet /norestart"} -ArgumentList $msiPath
+Invoke-Command -ComputerName  -Credentials  -ScriptBlock {Start-Process -FilePath $args[0] -ArgumentList "/X {88888888-FFFF-0000-FFFF-000055551111} /quiet /norestart"} -ArgumentList $msiPath
