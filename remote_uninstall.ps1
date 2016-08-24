84329a918ab9254b98510acd6ad018363b34a3be
Kay <k.dgray23@gmail.com>
2016-08-23 21:46:42 -0400

    Update remote_uninstall.ps1

--- remote_uninstall.ps1
+++ remote_uninstall.ps1
@@ -1,7 +1,7 @@
 ﻿$msiPath = "msiexec.exe"
 $msiargs = "MsiExec.exe /X{349560CC-4E99-4248-8E2C-299DEE142EA8} /quiet /norestart"
-Invoke-Command -ComputerName phl12578 -ScriptBlock {Start-Process -ArgumentList $using:msiargs}
+Invoke-Command -ComputerName  -ScriptBlock {Start-Process -ArgumentList $using:msiargs}
 #Start-Process $msiPath -ArgumentList $msiargs
-#Invoke-Command -ComputerName phl12578 -ScriptBlock {Get-ExecutionPolicy}
+#Invoke-Command -ComputerName  -ScriptBlock {Get-ExecutionPolicy}
 
 MsiExec.exe /X{349560CC-4E99-4248-8E2C-299DEE142EA8}
