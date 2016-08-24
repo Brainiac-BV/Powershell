68cd74b650b430a3aca33eb539939f6cde823dec
Kay <k.dgray23@gmail.com>
2016-08-23 21:47:43 -0400

    Update exchange_server_connection.ps1

--- exchange_server_connection.ps1
+++ exchange_server_connection.ps1
@@ -1,3 +1,3 @@
-﻿$ExchCHTserver = 'phlcht01'
+﻿$ExchCHTserver = ''
 $exchSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$ExchCHTserver/PowerShell/ -Authentication Kerberos 
 Import-PSSession $exchSession -AllowClobber
